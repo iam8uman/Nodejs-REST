e9b55a619b01b5721f4398d50b190ddee3ff7b77
iamsuman <suman.191544@ncit.edu.np>
2023-09-23 16:56:29 +0545

    Update

--- REST_EP03/src/helpers/index.ts
+++ REST_EP03/src/helpers/index.ts
@@ -1,5 +1,10 @@
 // this file is for encryption and decryption of the values
 
-import crypto from 'crypto'
+import crypto from "crypto";
 
-export const random=()=> crypto.randomBytes(128).toString('base64');//generate ramdom value for password encryption
+const Secret = "SUMAN_REST_API";
+
+export const random = () => crypto.randomBytes(128).toString("base64"); //generate ramdom value for password encryption
+export const athentication = (salt: String, password: String) => {
+  return crypto.createHmac("sha256", [salt, password].join('/')).update(Secret).digest("hex");
+};
